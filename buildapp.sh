e58634a4231787f1880212d6bc5712095bbf7b3b
qnblackcat <qnblackcat@gmail.com>
2022-03-22 12:19:21 +0700

    check file type

--- buildapp.sh
+++ buildapp.sh
@@ -13,18 +13,23 @@ else
 
 # Extract Cercube
 	echo -e "==> \033[1mExtracting Cercube...\033[0m"
-	if (cd Tweaks/Cercube && tar -xf me.alfhaily.cercube_5.3.7_iphoneos-arm.deb && tar -xf data.tar.*) ; then
+	if (cd Tweaks/Cercube && tar -xf me.alfhaily.cercube_5.3.7_iphoneos-arm.deb && tar -xf data.tar.*)
+then
 	echo -e "\033[1m> Extracted Cercube\033[0m"
 else
 	echo "> \033[1mCouldn't extract Cercube\033[0m"
 	fi
 
 # Makefile
-	read -e -p "==> Path to the decrypted YouTube IPA: " PATHTOIPA
+	read -e -p "==> Path to the decrypted YouTube iPA: " PATHTOIPA
+	if [[ $PATHTOIPA == *.ipa ]]
+then 
 	sed -i '' "14s#.*#CercubePlus_IPA = $PATHTOIPA#g" ./Makefile
 	make package
 	open ./packages
-
+else
+	echo "This is not an iPA!"
+	fi
 # Clean up	
 	tput setaf 1 && echo -e "==> \033[1mCleaning up...\033[0m"
 	find Tweaks/Cercube -mindepth 1 -name me.alfhaily.cercube_5.3.7_iphoneos-arm.deb -prune -o -exec rm -rf {} +
