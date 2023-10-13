3c72222399912ea2d26326db4225c30c0cce0df0
arichornlover <78001398+arichorn@users.noreply.github.com>
2023-10-12 21:47:07 -0500

    Add Extractor Files

---
+++ Source/Extractor.h
@@ -0,0 +1,7 @@
+// Extractor.h was made by @LillieH1000 from https://www.github.com/LillieH1000/YouTube-Reborn
+
+#import <Foundation/Foundation.h>
+
+@interface Extractor : NSObject
++ (NSDictionary *)youtubePlayerRequest :(NSString *)client :(NSString *)videoID;
+@end
