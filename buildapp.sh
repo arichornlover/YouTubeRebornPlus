d76ea142d88732eae20348b88ca0af8f7739d8e8
arichornlover <78001398+arichorn@users.noreply.github.com>
2023-12-15 12:57:01 -0600

    Update buildapp.sh

--- buildapp.sh
+++ buildapp.sh
@@ -3,17 +3,17 @@
 cd "$(dirname "$0")"
 
 # Check YouTube Reborn
-if	[ ! -f Tweaks/Reborn/YouTube.Reborn.v4.0.9.deb ]
+if	[ ! -f Tweaks/Reborn/YouTube.Reborn.v4.2.3.deb ]
 then
-	echo -e "==> \033[1mYouTube Reborn v4.0.9 is not found. Downloading YouTube Reborn (v4.0.9)...\033[0m"
-	curl https://dl.dropboxusercontent.com/s/8xv1vota8elfoml/YouTube.Reborn.v4.0.9.deb --output Tweaks/Reborn/YouTube.Reborn.v4.0.9.deb
+	echo -e "==> \033[1mYouTube Reborn v4.2.3 is not found. Downloading YouTube Reborn (v4.2.3)...\033[0m"
+	curl -L https://www.dropbox.com/scl/fi/ciejv4ph78e14oh8n5skl/YouTube.Reborn.4.2.3.deb?rlkey=cps2eiybbt6n6grm7vv5be5nm&dl=1 --output Tweaks/Reborn/YouTube.Reborn.v4.2.3.deb
 else
-   	echo -e "==> \033[1mFounded YouTube Reborn (v4.0.9)\033[0m"
+   	echo -e "==> \033[1mFounded YouTube Reborn (v4.2.3)\033[0m"
 fi
 
 # Extract Cercube
 	echo -e "==> \033[1mExtracting YouTube Reborn...\033[0m"
-if 	(cd Tweaks/Reborn && tar -xf YouTube.Reborn.v4.0.9.deb && tar -xf data.tar.*)
+if 	(cd Tweaks/Reborn && tar -xf YouTube.Reborn.v4.2.3.deb && tar -xf data.tar.*)
 then
 	echo -e "\033[1m> Extracted YouTube Reborn!\033[0m"
 else
@@ -50,6 +50,6 @@ fi
 
 # Clean up	
 	tput setaf 1 && echo -e "==> \033[1mCleaning up...\033[0m"
-	find Tweaks/Reborn -mindepth 1 ! -name "YouTube.Reborn.v4.0.9.deb" ! -name ".gitkeep" -exec rm -rf {} \; 2>/dev/null
+	find Tweaks/Reborn -mindepth 1 ! -name "YouTube.Reborn.v4.2.3.deb" ! -name ".gitkeep" -exec rm -rf {} \; 2>/dev/null
 	rm -rf tmp/ Resources .theos/_/Payload
 	echo -e  "==> \033[1mSHASUM256: $(shasum -a 256 packages/*.ipa | cut -f1 -d' ')\033[0m"
