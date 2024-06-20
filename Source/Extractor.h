7825948fc0fc19b3e24229f29c3601bded7c1179
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-06-19 19:49:20 -0500

    Update Extractor.h

--- Source/Extractor.h
+++ Source/Extractor.h
@@ -1,7 +1,7 @@
-// Extractor.h was made by @LillieH1000 from https://www.github.com/LillieH1000/YouTube-Reborn
+// Extractor.h was made by @LillieH1000 from https://github.com/LillieH1000/YouTube-Reborn/commit/bb858715235b1c2ce8793631bc34b51dfed491ef
 
 #import <Foundation/Foundation.h>
 
-@interface Extractor : NSObject
-+ (NSDictionary *)youtubePlayerRequest :(NSString *)client :(NSString *)videoID;
+@interface YouTubeExtractor : NSObject
++ (NSDictionary *)youtubePlayerRequest :(NSString *)clientId :(NSString *)videoId;
 @end
